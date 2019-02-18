1f14944ad49a1888f4b1ea887ed7139c829d4587
novikovrabota433 <32838534+novikovrabota433@users.noreply.github.com>
2019-02-18 14:35:29 +0300

    Add files via upload

---
+++ project/catalog.js
@@ -0,0 +1,102 @@
+$('.menu_open_0').mouseover(function() {
+	$('.sub-menu_main_0').slideToggle(200);
+});
+
+$('.menu_open_1').mouseover(function() {
+	$('.sub-menu_main_1').slideToggle(200);
+});
+
+$('.menu_open_2').mouseover(function() {
+	$('.sub-menu_main_2').slideToggle(200);
+});
+
+$('.menu_open_3').mouseover(function() {
+	$('.sub-menu_main_3').slideToggle(200);
+});
+
+$('.menu_open_4').mouseover(function() {
+	$('.sub-menu_main_4').slideToggle(200);
+});
+
+$('.menu_open_5').mouseover(function() {
+	$('.sub-menu_main_5').slideToggle(200);
+});
+
+$('.menu_open_6').mouseover(function() {
+	$('.sub-menu_main_6').slideToggle(200);
+});
+
+$('.menu_open_0').click(function() {
+	$('.sub-menu_main_1').css('display', 'none'),
+	$('.sub-menu_main_2').css('display', 'none'),
+	$('.sub-menu_main_3').css('display', 'none'),
+	$('.sub-menu_main_4').css('display', 'none'),
+	$('.sub-menu_main_5').css('display', 'none'),
+	$('.sub-menu_main_6').css('display', 'none');
+});
+
+$('.menu_open_1').click(function() {
+	$('.sub-menu_main_0').css('display', 'none'),
+	$('.sub-menu_main_2').css('display', 'none'),
+	$('.sub-menu_main_3').css('display', 'none'),
+	$('.sub-menu_main_4').css('display', 'none'),
+	$('.sub-menu_main_5').css('display', 'none'),
+	$('.sub-menu_main_6').css('display', 'none');
+});
+
+$('.menu_open_2').click(function() {
+	$('.sub-menu_main_0').css('display', 'none'),
+	$('.sub-menu_main_1').css('display', 'none'),
+	$('.sub-menu_main_3').css('display', 'none'),
+	$('.sub-menu_main_4').css('display', 'none'),
+	$('.sub-menu_main_5').css('display', 'none'),
+	$('.sub-menu_main_6').css('display', 'none');
+});
+
+
+$('.menu_open_3').click(function() {
+	$('.sub-menu_main_0').css('display', 'none'),
+	$('.sub-menu_main_1').css('display', 'none'),
+	$('.sub-menu_main_2').css('display', 'none'),
+	$('.sub-menu_main_4').css('display', 'none'),
+	$('.sub-menu_main_5').css('display', 'none'),
+	$('.sub-menu_main_6').css('display', 'none');
+});
+
+$('.menu_open_4').click(function() {
+	$('.sub-menu_main_0').css('display', 'none'),
+	$('.sub-menu_main_1').css('display', 'none'),
+	$('.sub-menu_main_2').css('display', 'none'),
+	$('.sub-menu_main_3').css('display', 'none'),
+	$('.sub-menu_main_5').css('display', 'none'),
+	$('.sub-menu_main_6').css('display', 'none');
+});
+
+$('.menu_open_5').click(function() {
+	$('.sub-menu_main_0').css('display', 'none'),
+	$('.sub-menu_main_1').css('display', 'none'),
+	$('.sub-menu_main_2').css('display', 'none'),
+	$('.sub-menu_main_3').css('display', 'none'),
+	$('.sub-menu_main_4').css('display', 'none'),
+	$('.sub-menu_main_6').css('display', 'none');
+});
+
+$('.menu_open_6').click(function() {
+	$('.sub-menu_main_0').css('display', 'none'),
+	$('.sub-menu_main_1').css('display', 'none'),
+	$('.sub-menu_main_2').css('display', 'none'),
+	$('.sub-menu_main_3').css('display', 'none'),
+	$('.sub-menu_main_4').css('display', 'none'),
+	$('.sub-menu_main_5').css('display', 'none');
+});
+
+
+
+
+
+
+
+$('.toggle-button_left').click(function() {
+	$('.section_left').css('display', 'block'),
+	$('.section_right').css('width', '100%');
+});
